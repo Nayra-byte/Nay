4d575e8bba4e4c128aef797522b6a04fd55efbbc
Nayra-byte <nayragoradia@gmail.com>
2025-10-26 11:04:38 +0530

    Add files via upload

---
+++ #User input.py
@@ -0,0 +1,10 @@
+#variable
+n=10
+g="Nayra G"
+print(n)
+print(g)
+GlazeCraze="GlazeCraze!"
+print(GlazeCraze)
+#User input
+name=input("Enter your name:")
+print("\nhello ",name,"\nWelcome to Glaze Craze")
