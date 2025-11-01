d1666a0aa2492959b80fa78eee029f56dc37efbb
Nayra-byte <nayragoradia@gmail.com>
2025-11-01 10:59:54 +0530

    Add files via upload

---
+++ data type.py
@@ -0,0 +1,8 @@
+a=2
+print("The data type of a is ",type(a))
+c=2.8
+print("The data type of c is ",type(c))
+b="🌸nayra"
+print("The data type of b is ",type(b))
+d=True
+print("The data type of d is ",type(d))
