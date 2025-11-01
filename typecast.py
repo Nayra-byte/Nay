64860b92052a17ebe54121c94b72a9f3d29b29fb
Nayra-byte <nayragoradia@gmail.com>
2025-11-01 11:14:32 +0530

    Add files via upload

---
+++ typecast.py
@@ -0,0 +1,20 @@
+#penguin
+name="Penguin"
+age=5
+is_student=True
+weight=20.2
+print("Name: ",name)
+print("The data type of name is ",type(name))
+print("Age: ",age)
+print("The data type of age is ",type(age))
+print("Weight: ",weight)
+print("The data type of weight is ",type(weight))
+print("is_student: ",is_student)
+print("The data type of is_student is ",type(is_student))
+print("\n after typecasting")
+age=str(age)
+print(age)
+print("The data type of age is ",type(age))
+weight=int(weight)
+print(weight)
+print("The data type of weight is ",type(weight))
