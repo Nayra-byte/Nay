722c3c3041a9d1df05170355d8cb72260796bf2e
Nayra-byte <nayragoradia@gmail.com>
2025-11-02 10:43:11 +0530

    Add files via upload

---
+++ AVERAGE 24568.py
@@ -0,0 +1,11 @@
+#adding
+t1=330897865
+t2=78435
+t3=98324
+t4=7983749  
+t5=36163198
+sum=t1+t2+t3+t4+t5
+print("The sum of all of the trees are : ",sum)
+#average
+average=sum/5
+print("The average of the trees are : ", average)
