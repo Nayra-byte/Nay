f87bffa5d06e5cd33dc4eaac31cb3c086da3078c
Nayra-byte <nayragoradia@gmail.com>
2025-11-22 11:23:48 +0530

    Add files via upload

---
+++ Num check!.py
@@ -0,0 +1,8 @@
+i= int(input("Enter a number: "))
+if (i<15):
+    print(i," is less than 15")
+    print("This is in the if block")
+else:
+    print(i," is greater than 15")
+    print("This is in the else block")
+print("I am not in the if or else block")
