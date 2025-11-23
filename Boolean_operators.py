bdf74d4ceeb5768c8c9225e4a39165321f0026e0
Nayra-byte <nayragoradia@gmail.com>
2025-11-23 10:47:39 +0530

    Add files via upload

---
+++ Boolean_operators.py
@@ -0,0 +1,20 @@
+a=0
+b=894509
+c=937509
+if a and b and c:
+    print("All the numbers have the boolean value as true")
+else:
+    print("At least one number has the boolean value as false")
+#variables
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("Neither of the numbers are greater than 0")
+#b>0 or c>0
+if b>0 or c>0:
+    print("Either of the numbers are greater than 0")
+else:
+    print("Neither of the numbers are greater than 0")
