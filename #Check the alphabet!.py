c4056d09dccbe65464cf3f4d4d6ed52cf38cde0a
Nayra-byte <nayragoradia@gmail.com>
2025-11-23 11:31:23 +0530

    Add files via upload

---
+++ #Check the alphabet!.py
@@ -0,0 +1,8 @@
+print("Enter a character: ")
+c=input()
+if c>='a' and c<='z':
+    print("\nIt is an alphabet")
+if c>='A' and c<='Z':
+    print("\nIt is an alphabet")
+else:
+    print("\nIt is not an alphabet")
