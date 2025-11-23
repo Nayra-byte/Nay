6c6add8b9eea36064401fe811294e6aaf5061550
Nayra-byte <nayragoradia@gmail.com>
2025-11-23 11:00:43 +0530

    Add files via upload

---
+++ #Not equal operator.py
@@ -0,0 +1,18 @@
+a=10
+b=12
+c=12
+print(a != b)
+print(b != c)
+a="python"
+b="coding"
+if a != b:
+    print("both a and b are different")
+a=4
+b=5
+if (a==1) != (b==5):
+    print("hello world, whats up?")
+a= int(input("Enter a number: "))
+if a%2!=0:
+    print("It is an odd number")
+else:
+    print("it is an even number")
