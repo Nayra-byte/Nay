878a98c17984ba7669ab7e4c4b2dc322f70b45d0
Nayra-byte <nayragoradia@gmail.com>
2025-12-21 11:14:34 +0530

    Add files via upload

---
+++ #########################################calculator.py
@@ -0,0 +1,26 @@
+def add(p,q):
+    return p+q
+def subtract(p,q):
+    return p-q
+def multiply(p,q):
+    return p*q
+def divide(p,q):
+    return p/q
+print("Choose your operation")
+print("a.addition")
+print("b.subtraction")
+print("c.multiplication")
+print("d.division")
+choice=str(input("Please enter your choice - a/b/c/d"))
+n1=int(input("Enter your first number: "))
+n2=int(input("Enter your second number: "))
+if choice=='a':
+    print(n1," + ",n2, " = ",add(n1,n2))
+elif choice=='b':
+    print(n1," - ",n2, " = ",subtract(n1,n2))
+elif choice=='c':
+    print(n1," * ",n2, " = ",multiply(n1,n2))
+elif choice=='d':
+    print(n1," / ",n2, " = ",divide(n1,n2))
+else:
+    print("Invalid input")
