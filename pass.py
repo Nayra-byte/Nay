5b16799ea7923323c7ab4c6e7d60c7876dd1517d
Nayra-byte <nayragoradia@gmail.com>
2026-01-04 11:09:21 +0530

    Add files via upload

---
+++ pass.py
@@ -0,0 +1,11 @@
+for n in range(999):
+    if n %20 ==0:
+        print("twist")
+    elif n %15 ==0:
+        pass
+    elif n %5 ==0:
+        print("fizz")
+    elif n %3 ==0:
+        print("buzz")
+    else:
+        print(n)
