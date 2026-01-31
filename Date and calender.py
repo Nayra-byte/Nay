1955a8929da647621fdcc2d4b6671f4455c46725
Nayra-byte <nayragoradia@gmail.com>
2026-01-31 10:55:51 +0530

    Add files via upload

---
+++ Date and calender.py
@@ -0,0 +1,6 @@
+from datetime import date, time, datetime
+today = date.today()
+now= datetime.now()
+print("The date today is ", today)
+print("The time and date right now is ", now)
+print("The date components are ", today.day, today.month, today.year)
