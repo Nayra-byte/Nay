91177d51226da23734d0847de9473c2742375b56
Nayra-byte <nayragoradia@gmail.com>
2026-02-08 11:20:56 +0530

    Add files via upload

---
+++ countrycode.py
@@ -0,0 +1,5 @@
+country_code={'India':'7865','Indonesia':'8898','Australia':'7654','Korea':'7890','Japan':'9990'}
+print("country code for India-")
+print(country_code.get('India','not found'))
+print("country code for Namebia-")
+print(country_code.get('Namebia','not found'))
