33de2fac0f5faa7e0d5248ee8817438e44bedd4a
Nayra-byte <nayragoradia@gmail.com>
2026-02-14 11:03:55 +0530

    Add files via upload

---
+++ identify meeeeeeeeeeeeeeeeeeeeeeeeeeee!!!!!!!!!!!.py
@@ -0,0 +1,8 @@
+setx={'purple','blue'}
+sety={'purple','yellow'}
+print('Original set elements-')
+print(setx)
+print(sety)
+print('Intersection of 2 sets')
+setz=sety.intersection(setx)
+print(setz)
