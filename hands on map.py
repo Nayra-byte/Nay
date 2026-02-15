c22a2d529f7f66fdb436c4cb5757c07168b8365f
Nayra-byte <nayragoradia@gmail.com>
2026-02-15 10:56:56 +0530

    Add files via upload

---
+++ hands on map.py
@@ -0,0 +1,11 @@
+n1=[1,2,3]
+n2=[4,5,6]
+r=map(lambda x,y:x+y,n1,n2)
+print("addition of 2 lists ")
+print(list(r))
+ns=[1,2,3,4,5]
+def sq(n):
+    return n*n
+square=list(map(sq,ns))
+print('square of numbers in the list')
+print(square)
