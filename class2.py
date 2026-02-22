b41342acb3eb7cbf56591f9eb6ffabd395f0f64a
Nayra-byte <nayragoradia@gmail.com>
2026-02-22 11:09:57 +0530

    Add files via upload

---
+++ class2.py
@@ -0,0 +1,8 @@
+#hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+class vehicle:
+    def __init__ (self,max_speed, mileage):
+        self.max_speed=max_speed
+        self.mileage=mileage
+modelX=vehicle(240,16)
+print('Model max speed ', modelX.max_speed)
+print('Model mileage ', modelX.mileage)
